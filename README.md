0b5915c89f7731ef61f43e818e4b4c3e9b5da533
Herivelton Andreassa <handreassa@gmail.com>
2023-02-12 23:48:33 -0300

    Initial commit - adding initial logic for login

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# prenotami
-Python bot to log into prenot@mi and schedule citizenship/passport
+# Prenotami schedule helper
+Python bot to log into [prenot@mi](prenotami.esteri.it) and schedule citizenship/passport using Selenium.
+
+How to use it:
+Download this repo and create a virtual env. Make use of it and install the required libs.
+```
+git clone https://github.com/handreassa/prenotami
+cd prenotami
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+Modify the parameters.yaml file to fit your specific needs.
---
+++ __main__.py
@@ -0,0 +1,76 @@
+import os
+from dotenv import load_dotenv
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+import logging
+import yaml
+load_dotenv()
+
+logging.basicConfig(
+    format = '%(levelname)s:%(message)s',
+    level = logging.INFO,
+    handlers = [
+        logging.FileHandler('/tmp/out.log'),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+class Prenota:
+    def __init__(self):
+        self.user_config = load_config('parameters.yaml')
+        self.email = os.getenv('username')
+        self.password = os.getenv('password')
+        self.driver = webdriver.Safari()
+
+
+    def check_file_exists(file_name):
+        parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+        file_path = os.path.join(parent_dir, file_name)
+        return os.path.isfile(file_path)
+
+    def load_config(file_path):
+        # Open the YAML file
+        with open(file_path, 'r') as file:
+            # Load the YAML content into a Python dictionary
+            config = yaml.safe_load(file)
+        return config
+
+    def do_login(self):
+        try:
+            self.driver.get("https://prenotami.esteri.it/")
+            # Wait for the page to fully load
+            self.driver.implicitly_wait(5)
+            email_box = WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.ID, "login-email")))
+            password_box = self.driver.find_element(By.ID, "login-password")
+            email_box.send_keys(self.email)
+            password_box.send_keys(self.password)
+        except Exception as e:
+            logging.info(f"Exception: {e}")
+
+    def fill_form():
+        ...
+
+    if __name__ == "__main__":
+        
+        do_login(self)
+
+        if request_type == 'citizenship':
+            self.driver.get('https://prenotami.esteri.it/Services/Booking/751')
+            appts_available = self.driver.find_element(By.XPATH, "//*[@id='WlNotAvailable']").get_attribute("value")
+            
+            if appts_available == 'Al momento non ci sono date disponibili per il servizio richiesto':
+                logging.info("TIMESTAMP: " + str(datetime.now()))
+                logging.info("No change to prenotami detected.")
+                sys.exit(0)
+            else:
+                message = "Change detected on prenotami, please check system for available appointments."
+                logging.info("TIMESTAMP: " + str(datetime.now()))
+                logging.info("Change detected on prenotami.")
+                fill_form(self)
+            
+        # Close the driver once the page has loaded
+        driver.quit()
